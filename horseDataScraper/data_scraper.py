cfc812cef858e6ca803979f247e86fb8d2e5e032
3l4l5 <csru20003@g.nihon-u.ac.jp>
2022-05-28 15:42:51 +0900

    修正

--- horseDataScraper/data_scraper.py
+++ horseDataScraper/data_scraper.py
@@ -2,10 +2,13 @@ from horse.src import autoHorseScraper
 from race.src import autoRaceScraper
 from peds.src import autoPedsScraper
 
+IS_TEST = True
+IS_DRY_RUN = False
+
 if __name__ == "__main__":
     print("scraping race html")
-    autoRaceScraper.race_scraper(is_test=True, is_dry_run=False)
+    autoRaceScraper.race_scraper(is_test=IS_TEST, is_dry_run=IS_DRY_RUN)
     print("scraping horse html")
-    autoHorseScraper.horse_scraper(is_test=True, is_dry_run=False)
+    autoHorseScraper.horse_scraper(is_test=IS_TEST, is_dry_run=IS_DRY_RUN)
     print("scraping peds html")
-    autoPedsScraper.peds_scraper(is_test=True, is_dry_run=False)
+    autoPedsScraper.peds_scraper(is_test=IS_TEST, is_dry_run=IS_DRY_RUN)
